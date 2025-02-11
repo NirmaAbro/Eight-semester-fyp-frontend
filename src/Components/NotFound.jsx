8bf03cec8d484b2f70f956fb9ff1164cfd399707
Nirma Abro <NirmaAbro143@gmail.com>
2025-02-11 14:19:13 -0600

    page-not-found-bg-changed

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -4,13 +4,13 @@ import { NavLink } from "react-router-dom";
 function NotFound() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="max-w-lg mx-auto p-8 bg-white shadow-lg rounded-lg text-center">
-        <h1 className="text-4xl lg:text-6xl font-bold text-gray-800 mb-8">
+      <div className="max-w-lg mx-auto p-8 bg-primary shadow-lg rounded-lg text-center">
+        <h1 className="text-4xl lg:text-6xl font-bold text-white mb-8">
           Oops! Page Not Found
         </h1>
         <NavLink
           to="/"
-          className="inline-block bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-6 rounded transition duration-300"
+          className="inline-block bg-white text-primary font-bold py-3 px-6 rounded transition duration-300"
         >
           Go to Home Page
         </NavLink>
